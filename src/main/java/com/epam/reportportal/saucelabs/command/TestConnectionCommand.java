2d8ebfe9a84c7086aa75296938b1ca5c34c01e20
Evelina Sarkisian <59954853+Evelina02@users.noreply.github.com>
2026-03-10 12:42:59 +0300

    EPMRPP-110768 fix paths (#50)

--- src/main/java/com/epam/reportportal/saucelabs/command/TestConnectionCommand.java
+++ src/main/java/com/epam/reportportal/saucelabs/command/TestConnectionCommand.java
@@ -18,9 +18,9 @@ package com.epam.reportportal.saucelabs.command;
 
 import static com.epam.reportportal.saucelabs.model.Constants.TEST_CONNECTION;
 
-import com.epam.reportportal.infrastructure.persistence.entity.integration.Integration;
-import com.epam.reportportal.infrastructure.rules.exception.ErrorType;
-import com.epam.reportportal.infrastructure.rules.exception.ReportPortalException;
+import com.epam.reportportal.base.infrastructure.persistence.entity.integration.Integration;
+import com.epam.reportportal.base.infrastructure.rules.exception.ErrorType;
+import com.epam.reportportal.base.infrastructure.rules.exception.ReportPortalException;
 import com.epam.reportportal.saucelabs.client.RestClientBuilder;
 import com.epam.reportportal.saucelabs.model.IntegrationProperties;
 import com.epam.reportportal.saucelabs.utils.ValidationUtils;
